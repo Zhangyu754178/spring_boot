6ee33ba6e5b0022ca223e9414a8514b0808f73b0
zhangyu <18101069278@163.com>
2019-02-13 17:23:41 +0800

    no message

---
+++ demo/src/main/java/com/example/demo/controller/DbController.java
@@ -0,0 +1,86 @@
+package com.example.demo.controller;
+
+import com.example.demo.been.User;
+import com.example.demo.inter.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.*;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.List;
+
+@RestController
+public class DbController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+//    @RequestMapping("/db")
+//    void SaveUser() {
+//        Date date = new Date();
+//        DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG);
+//        String formattedDate = dateFormat.format(date);
+//
+//        userRepository.save(new User("aa1", "aa@126.com", "aa", "aa123456",formattedDate));
+//
+//        //userRepository.delete(userRepository.findByUserName("aa1"));
+//    }
+
+
+    @GetMapping(value = "/person/all")
+    private List<User> personList() {
+        return userRepository.findAll();
+    }
+
+
+    @PostMapping(value = "/person")
+    public User personAdd(@RequestParam("name") String name,
+                            @RequestParam("password") String password,
+                          @RequestParam("email") String email,
+                          @RequestParam("nickname") String nickname) {
+        Date date = new Date();
+        DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG);
+        String formattedDate = dateFormat.format(date);
+        User person = new User(name,password,email,nickname,formattedDate);
+        return userRepository.save(person);
+    }
+    /**
+     * 查询一个人员
+     *
+     * @param name
+     * @return
+     */
+    @GetMapping(value = "/person/{name}")
+    public User personFindOne(@PathVariable("name") String name) {
+        return userRepository.findByUserName(name);
+    }
+
+    /**
+     * 删除一个人员
+     *
+     * @param name
+     */
+    @DeleteMapping(value = "/person/{name}")
+    public void personDelete(@PathVariable("name") String name) {
+        userRepository.delete(userRepository.findByUserName(name));
+    }
+
+    /**
+     * 更新一个人员
+     *
+
+     * @return
+     */
+    @PutMapping(value = "/person/{name}")
+    public User personUpdate(@RequestParam("name") String name,
+                             @RequestParam("password") String password,
+                             @RequestParam("email") String email,
+                             @RequestParam("nickname") String nickname) {
+        Date date = new Date();
+        DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG);
+        String formattedDate = dateFormat.format(date);
+        User person = new User(name,password,email,nickname,formattedDate);
+        return userRepository.save(person);
+    }
+}
---
+++ demo/src/main/java/com/example/demo/inter/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.inter;
+
+import com.example.demo.been.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUserName(String userName);
+    User findByUserNameOrEmail(String username, String email);
+}
